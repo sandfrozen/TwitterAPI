3e476cb5b2df19c386bcec6a989d2f4f47d646db
Tomek Buslowski <tbuslowski@gmail.com>
2018-03-06 16:39:07 +0100

    PSQL configured

---
+++ TwitterAPI/Models/TwitterAPIContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace TwitterAPI.Models
+{
+    public class TwitterAPIContext : DbContext
+    {
+
+        public TwitterAPIContext(DbContextOptions<TwitterAPIContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
